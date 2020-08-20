48e8d5f80d0cafe75c598ae001528c96bdfe1431
mohan.tadikimalla <mohan.tadikimalla@fireeye.com>
2020-08-20 20:37:22 +0530

    fineshed auth flow

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const authRoutes = require("./routes/authRoutes");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
-app.get("/", (req, res) => {
-    res.send({ hi: "there mohan test" });
-});
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 1000,
+        keys: [keys.cookieKey],
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
